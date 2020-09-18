ea19bb252ed470c3d3e23a980180a6d1bb07a146
Andrey Krushelnitskiy <andrey.krushelnitsky@myoffice.team>
2020-09-18 10:46:28 +0300

    add full path

--- nginx_reload/nginxReloader.sh
+++ nginx_reload/nginxReloader.sh
@@ -2,12 +2,12 @@
 
 while true
 do
- inotifywait --exclude .swp -r -e create -e modify -e delete -e move -e close /etc/nginx/conf.d
- nginx -t
- if [ $? -eq 0 ]
- then
-  echo "Detected Nginx Configuration Change"
-  echo "Executing: nginx -s reload"
-  nginx -s reload
- fi
+  /usr/bin/inotifywait --exclude .swp -r -e create -e modify -e delete -e move -e close /etc/nginx/conf.d
+  /usr/sbin/nginx -t
+  if [ $? -eq 0 ]
+  then
+    echo "Detected Nginx Configuration Change"
+    echo "Executing: nginx -s reload"
+    /usr/sbin/nginx -s reload
+   fi
 done
